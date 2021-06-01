8ede767819cd2ddd40f193d65038bae58aebb677
Boris Yelnikoff <b.zawada84@gmail.com>
2021-06-01 20:04:24 +0200

    Build sticky menu.

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('Working');
+const collapsible = document.querySelector(".collapsible");
+collapsible.addEventListener("click", function () {
+  this.classList.toggle("collapsible--expanded");
+});
+
+window.addEventListener("scroll", handleScroll);
+
+const navBar = document.querySelector(".navbar");
+const hero = document.querySelector(".hero");
+const content = document.querySelector(".content");
+
+function handleScroll() {
+  if (window.scrollY > hero.offsetHeight) {
+    navBar.classList.add("sticky");
+    content.classList.add("sticky-content");
+    collapsible.classList.remove("collapsible--expanded");
+  } else {
+    navBar.classList.remove("sticky");
+    content.classList.remove("sticky-content");
+  }
+}
